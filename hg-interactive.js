b18f8de913f6ad66b8a3edc5cb9293d5e9d3a455
Soren Harward <soren.harward@uspto.gov>
2022-12-19 00:01:13 -0500

    Made the database listing easily extensible

--- hg-interactive.js
+++ hg-interactive.js
@@ -6,32 +6,31 @@ function generateHistoryPage() {
       entries.push(JSON.parse(f[i].value));
     }
   }
-  console.log("generating a table with", entries);
+  //console.log("generating a table with", entries);
 
   entries.sort(function(a,b){return a.lastvisit - b.lastvisit});
 
   var h = document.createElement('h1');
-  h.textContent = "Prior Art Web Search History";
+  h.textContent = "Web Search History";
   document.getElementById('printable_div').appendChild(h);
   
   var t = document.createElement('table');
   h = t.createTHead();
   hr = h.insertRow(0);
   hc = hr.insertCell(0);
-  hc.textContent = 'date/time';
+  hc.textContent = 'date, time';
   hc = hr.insertCell(1);
   hc.textContent = 'web site';
   hc = hr.insertCell(2);
-  hc.textContent = 'query';
+  hc.textContent = 'search string';
   for (var i = 0; i < entries.length; ++i) {
     r = t.insertRow(i+1);
     tc = r.insertCell(0);
     tc.textContent = entries[i]['timestamp'];
     tc = r.insertCell(1);
-    tc.textContent = "database";
+    tc.textContent = entries[i]['website'];
     tc = r.insertCell(2);
-    tc.textContent = entries[i]['title'];
-    //tc.textContent = historyData[i]['search string'];
+    tc.textContent = entries[i]['query'];
   }
   document.getElementById('printable_div').appendChild(t);
 
--- hg-setup.js
+++ hg-setup.js
@@ -1,8 +1,17 @@
-function renderHistoryTable(divName, historyData) {
-  //TODO: make the table look nicer
+function renderHistoryTable(divName, website, historyData) {
+  d = document.getElementById(divName);
+
   var t = document.createElement('table');
+  h = t.createTHead();
+  hr = h.insertRow(0);
+  hc = hr.insertCell(0);
+  //TODO: have a checkbox that allows checking/unchecking of all entries
+  hc.textContent = '';
+  hc = hr.insertCell(1);
+  hc.textContent = "searches on " + website;
+
   for (var i = 0; i < historyData.length; ++i) {
-    r = t.insertRow(i);
+    r = t.insertRow(i+1);
     tc = r.insertCell(0);
     tc.textContent = "";
     cb = document.createElement('input');
@@ -11,52 +20,70 @@ function renderHistoryTable(divName, historyData) {
     cb.checked = true;
     tc.appendChild(cb);
     tc = r.insertCell(1);
-    tc.textContent = historyData[i]['title'];
+    tc.textContent = historyData[i]['query'];
     tc = r.insertCell(2);
     tc.textContent = historyData[i]['timestamp']
   }
-  document.getElementById(divName).appendChild(t);
-  document.getElementById(divName).appendChild( document.createElement('hr') );
+  d.appendChild(t);
+  d.appendChild( document.createElement('hr') );
 }
 
-function makeHistoryItemProcessor(divName) {
+function makeHistoryItemProcessor(divName, website, queryExtractionF) {
   return function historyItemProcessor(historyItems) {
     var tableEntries = [];
-    //console.log("processing", historyItems.length, "history items", historyItems);
     for (var i = 0; i < historyItems.length; ++i) {
-      // FIXME: clean up the title
-      // FIXME: find the first visit, not the most recent
+      // TODO: find the first visit, not the most recent
       var e = {
+        'website': website,
         'url': historyItems[i].url,
-        'title': historyItems[i].title,
+        'query': queryExtractionF(historyItems[i]),
         'lastvisit': historyItems[i].lastVisitTime,
         'timestamp': (new Date(historyItems[i].lastVisitTime)).toLocaleString(),
         'id': historyItems[i].id
       };
-      tableEntries.push(e);
+      if (e['query']) {
+        tableEntries.push(e);
+      }
     }
-    //console.log("history list contains", tableEntries.length, "history items");
-    renderHistoryTable(divName, tableEntries);
+    renderHistoryTable(divName, website, tableEntries);
   };
 }
 
 function buildAvailableHistoryList(divName) {
   // TODO: make this configurable
-  var cutoffTime = (new Date).getTime() - (1000 * 60 * 60 * 24 * 90)
+  var cutoffTime = (new Date).getTime() - (1000 * 60 * 60 * 24 * 90);
 
-  //FIXME: search the history for the correct databases
-  chrome.history.search({
-      'startTime': cutoffTime, // starting with the specified cutoff time ...
-      'text': 'wikipedia'      // ... return all results from wikipedia
+  // clear the existing entries
+  document.getElementById(divName).textContent = '';
+
+  websites = [
+    {'website':'Google Books',
+      'matchpattern':'tbm=bks',
+      'queryextractor':function(h) { return h.title.slice(0,-15); }
     },
-    makeHistoryItemProcessor(divName) // this is a closure
-  );
-  chrome.history.search({
-      'startTime': cutoffTime, // starting with the specified cutoff time ...
-      'text': 'stackoverflow'  // ... return all results from stackoverflow
+    {'website':'Google Images',
+      'matchpattern':'tbm=isch',
+      'queryextractor':function(h) { return h.title.slice(0,-15); }
     },
-    makeHistoryItemProcessor(divName) // this is a closure
-  );
+    {'website':'Google Scholar',
+      'matchpattern':'scholar.google.com',
+      'queryextractor':function(h) { return h.title.slice(0,-17); }
+    },
+    {'website':'Google Patents',
+      'matchpattern':'tbm=pts',
+      'queryextractor':function(h) { return h.title.slice(0,-16); }
+    }
+  ]
+
+  for (var i = 0; i < websites.length; i++) {
+    w = websites[i];
+    chrome.history.search({
+        'startTime': cutoffTime,  // starting with the specified cutoff time ...
+        'text': w['matchpattern'] // ... return all results from the website
+      },
+      makeHistoryItemProcessor(divName, w['website'], w['queryextractor']) // this is a closure
+    );
+  }
 }
 
 document.addEventListener('DOMContentLoaded',
