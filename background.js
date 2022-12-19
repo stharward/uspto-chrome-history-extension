57d786a2d544d94b1314fa6f8ed43999d1e7eed4
Soren Harward <soren.harward@uspto.gov>
2022-12-19 00:01:17 -0500

    port to manifest V3

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 /*
-Written by Soren Harward <soren.harward@uspto.gov>, May 2015
+Written by Soren Harward <soren.harward@uspto.gov>, 2015-2021
 
 This software was made by an employee of the United States Government as part
 of his official duties. As such, it is not subject to copyright in the United
@@ -27,8 +27,8 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-chrome.browserAction.onClicked.addListener(
-    function() {
-      chrome.tabs.create({url:chrome.extension.getURL("historyGenerator.html")});
-    }
+chrome.action.onClicked.addListener(
+  function() {
+    chrome.tabs.create({url:chrome.runtime.getURL("historyGenerator.html")});
+  }
 );
